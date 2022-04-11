1cf4331f4097f256b2b34638c5ea8c0a18a76d68
Abdulrhman H <aherzallah@cerebra.sa>
2022-04-11 23:43:49 +0300

    create Blockchain constructor function & createNewBlock method

---
+++ dev/blockchain.js
@@ -0,0 +1,23 @@
+// create Blockchain constructor function it is like a class object
+
+function Blockchain(){
+    this.chain = [];
+    this.newTransactions = [];
+}
+
+
+Blockchain.prototype.CreateNewBlock = function (nonce, previousBlockHash, hash) {
+    const newBlock = {
+        index : this.chain.length + 1, // id from new block
+        timestamp : Date.now(), // when create new block
+        transactions : this.newTransactions, // get all transactions history
+        nonce : nonce, // comes from proof of work, simply just number, we create block from legitimate way, by using proof of work method
+        hash : hash, // data from our new block | current hash |
+        previousBlockHash : previousBlockHash, // get previous block hash
+    }
+
+    this.newTransactions = []; // clear all transactions | the newBlock have all transactions
+    this.chain.push(newBlock); // push new block to new chain
+
+    return newBlock;
+}
