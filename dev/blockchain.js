9a73cd5fdb2bca2df83923c9c3cc88e9e6e31528
Abdulrhman H <aherzallah@cerebra.sa>
2022-04-12 00:27:03 +0300

    create new block method & edit new newTransactions array to pendingTransactions

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -2,20 +2,20 @@
 
 function Blockchain(){
     this.chain = [];
-    this.newTransactions = [];
+    this.pendingTransactions = [];
 }
 
 Blockchain.prototype.CreateNewBlock = function (nonce, previousBlockHash, hash) {
     const newBlock = {
         index : this.chain.length + 1, // id from new block
         timestamp : Date.now(), // when create new block
-        transactions : this.newTransactions, // get all transactions history
+        transactions : this.pendingTransactions, // get all transactions history
         nonce : nonce, // comes from proof of work, simply just number, we create block from legitimate way, by using proof of work method
         hash : hash, // data from our new block | current hash |
         previousBlockHash : previousBlockHash, // get previous block hash
     }
 
-    this.newTransactions = []; // clear all transactions | the newBlock have all transactions
+    this.pendingTransactions = []; // clear all transactions | the newBlock have all transactions
     this.chain.push(newBlock); // push new block to new chain
 
     return newBlock;
@@ -24,6 +24,20 @@ Blockchain.prototype.CreateNewBlock = function (nonce, previousBlockHash, hash) 
 // method to get last block
 Blockchain.prototype.getLastBlock = function () {
     return this.chain[this.chain.length - 1]
+}
+
+// method to create new transaction
+Blockchain.prototype.CreateNewTransaction = function (amount, sender, recipient){
+    const newTransaction = {
+      amount : amount, // amount which will be a mount's of the transactions or how much is being sent is this
+      sender : sender, // sender address
+      recipient : recipient, // recipient address
+    };
+
+    // push new transaction to newTransactions array
+    this.pendingTransactions.push(newTransaction)
+
+    return this.getLastBlock()['index'] + 1 // get last block in the chain
 }
 
 module.exports = Blockchain
