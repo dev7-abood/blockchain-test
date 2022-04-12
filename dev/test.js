a8b30e19c18ee844ec0f6298ba970484b2e7d31c
Abdulrhman H <aherzallah@cerebra.sa>
2022-04-12 22:47:19 +0300

    file test of work methods

--- dev/test.js
+++ dev/test.js
@@ -1,8 +1,4 @@
 const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
-bitcoin.CreateNewBlock(22552, 'SDASDASDAASDASDQWFDFADDDDDD', 'SDASDASDAASDASDQWFDFAFDFDFF')
-bitcoin.CreateNewBlock(81515, 'SDASDASDFDFSDFAASDASDQWFDFA', 'SDASDASDAASDASDQWFDFADDDDDD')
-bitcoin.CreateNewBlock(56020, 'dasd1ASDA5SDFSD4DA4SD5A4SD6A5SDAS', 'SDASDASDFDFSDFAASDASDQWFDFA')
-
 console.log(bitcoin)
